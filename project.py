c2bd7a1f01be142d70d5aba601bde1d544ee0bdc
shivasupraj <shivasupraj.p@gmail.com>
2018-12-21 21:25:36 -0600

    completed the crud operations for the menu item module

--- project.py
+++ project.py
@@ -110,19 +110,112 @@ def deleteRestaurant(restaurant_id):
 
 @app.route('/restaurants/<int:restaurant_id>/menu')
 def restaurantMenu(restaurant_id):
+    sqlalchemy_obj = session.query(Restaurant).filter_by(id = restaurant_id)
+    if sqlalchemy_obj.count() == 0:
+        flash('No menu available for this restaurant')
+        return redirect(url_for('restaurantsList'))
+    restaurant = sqlalchemy_obj.one()
+    sqlalchemy_obj = session.query(MenuItem).filter_by(restaurant = restaurant)
+    if sqlalchemy_obj.count() == 0:
+        flash('No menu available for this restaurant')
+        return redirect(url_for('restaurantsList'))
+    items = sqlalchemy_obj
     return render_template('menu.html', items = items, restaurant = restaurant)
 
-@app.route('/restaurants/<int:restaurant_id>/menu/new')
+@app.route('/restaurants/<int:restaurant_id>/menu/new', methods = ['GET', 'POST'])
 def addMenuItem(restaurant_id):
-    return render_template('newMenuItem.html', restaurant = restaurant)
+    if request.method == 'GET':
+        sqlalchemy_obj = session.query(Restaurant).filter_by(id = restaurant_id)
+        if sqlalchemy_obj.count() == 0:
+            flash('No menu available for this restaurant')
+            return redirect(url_for('restaurantsList'))
+        restaurant = sqlalchemy_obj.one()
+        return render_template('newMenuItem.html', restaurant = restaurant)
+    else:
+        sqlalchemy_obj = session.query(Restaurant).filter_by(id = restaurant_id)
+        if sqlalchemy_obj.count() == 0:
+            flash('No menu available for this restaurant')
+            return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+        else:
+            restaurant = sqlalchemy_obj.one()
+        form = request.form
+        if len(form) > 0 and 'menu_name' in form:
+            menu_name = form['menu_name']
+        else:
+            flash('Cannot add the menu item to menu')
+            return redirect(url_for('restaurantsList'))
+        description = form['description'] if 'description' in form else None
+        price =  '$' + str(form['price']) if 'price' in form else None
+        course = form['course'] if 'course' in form else None
+        menu_item = MenuItem(name = menu_name, description = description, price = price, course = course, restaurant = restaurant)
+        session.add(menu_item)
+        session.commit()
+        flash('Added the menu item to the menu')
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
 
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit')
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit', methods = ['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return render_template('editMenuItem.html', restaurant = restaurant, item = item)
+    if request.method == 'GET':
+        sqlalchemy_obj = session.query(Restaurant).filter_by(id = restaurant_id)
+        if sqlalchemy_obj.count() == 0:
+            flash('Could not find the restaurant')
+            return redirect(url_for('retaurantsList'))
+        restaurant = sqlalchemy_obj.one()
+        sqlalchemy_obj = session.query(MenuItem).filter_by(id = menu_id)
+        if sqlalchemy_obj.count() == 0:
+            flash('Could not find the menu')
+            return redirect(url_for('restaurantsList'))
+        item = sqlalchemy_obj.one()
+        return render_template('editMenuItem.html', restaurant = restaurant, item = item)
+    else:
+        sqlalchemy_obj = session.query(MenuItem).filter_by(id = menu_id)
+        if sqlalchemy_obj.count() == 0:
+            flash('Could not update the menu item to the menu')
+            return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+        menu = sqlalchemy_obj.one()
+        form = request.form
+        if len(form) == 0:
+            flash('Could not update the menu item to the menu')
+            return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+        print(menu.name)
+        if 'menu_name' in form:
+            menu.name = form['menu_name'] 
+        if 'description' in form:
+            menu.description = form['description'] 
+        if 'price' in form:
+            menu.price =  '$' + str(form['price']) 
+        if 'course' in form:
+            menu.course = form['course'] 
+        session.add(menu)
+        session.commit()
+        flash('updated the menu item to the menu')
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
 
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete')
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete', methods = ['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return render_template('deleteMenuItem.html', item = item, restaurant = restaurant)
+    if request.method == 'GET':
+        sqlalchemy_obj = session.query(Restaurant).filter_by(id = restaurant_id)
+        if sqlalchemy_obj.count() == 0:
+            flash('Could not load the restaurant')
+            return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+        restaurant = sqlalchemy_obj.one()
+        sqlalchemy_obj = session.query(MenuItem).filter_by(id = menu_id)
+        if sqlalchemy_obj.count() == 0:
+            flash('Could not load the menu item')
+            return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+        item = sqlalchemy_obj.one()
+        return render_template('deleteMenuItem.html', item = item, restaurant = restaurant)
+    else:
+        sqlalchemy_obj = session.query(MenuItem).filter_by(id = menu_id)
+        if sqlalchemy_obj.count() == 0:
+            flash('Could not delete the menu item')
+            return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+        item = sqlalchemy_obj.one()
+        session.delete(item)
+        session.commit()
+        flash('Deleted the menu item successfully')
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
 
 
 if __name__ == '__main__':
